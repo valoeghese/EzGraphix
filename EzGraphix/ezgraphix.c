8fab0042d206c3da7c1f0648af1a796271bb36a8
Valoeghese <valoeghesemods@gmail.com>
2022-10-10 14:54:49 +1300

    colour tinting

--- EzGraphix/ezgraphix.c
+++ EzGraphix/ezgraphix.c
@@ -476,7 +476,7 @@ int main(void) {
 
 		// If Texture, use that
 		"  if (hasTexture) {\n"
-		"    gl_FragColor = texture(textureSampler, uvPass);\n"
+		"    gl_FragColor = texture(textureSampler, uvPass) * vec4(colour, 1.0);\n"
 		"  } else {\n"
 		// Else, use colour
 		"    gl_FragColor = vec4(colour, 1.0);\n"
